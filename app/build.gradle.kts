2fadd6b9ca82b7b6ed95c49f9663fb3c8e449cab
Spencer Mendelsohn <spmendelsohn1@gmail.com>
2025-06-23 16:38:15 -0400

    tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    // Mockito for mocking
+    testImplementation("org.mockito:mockito-core:5.2.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
 }
